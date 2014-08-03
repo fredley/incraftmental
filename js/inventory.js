3ab1da8ba80709b88b84bddda573d103a044140b
fredley <fredley@gmail.com>
2014-08-03 16:53:01 +0000

    String and milk are now obtainable

--- js/inventory.js
+++ js/inventory.js
@@ -68,10 +68,10 @@ var inventory = {
 
     tools : {
       wooden_sword :    {display:'Wooden Sword',    profession: 'adventurer', recipe: 'P  P  |' , bonus:1, gives: {'porkchop':0.05}                                                                              },
-      stone_sword :     {display:'Stone Sword',     profession: 'adventurer', recipe: 'C  C  |' , bonus:2, gives: {'porkchop':0.1, 'egg':0.1}                                                                    },
-      iron_sword :      {display:'Iron Sword',      profession: 'adventurer', recipe: 'i  i  |' , bonus:3, gives: {'leather':0.1, 'porkchop':0.1, 'egg':0.1, 'feather':0.1}                                      },
-      gold_sword :      {display:'Golden Sword',    profession: 'adventurer', recipe: 'g  g  |' , bonus:4, gives: {'leather':0.1, 'porkchop':0.1, 'egg':0.1, 'feather':0.1, 'wool':0.1}                          },
-      diamond_sword :   {display:'Diamond Sword',   profession: 'adventurer', recipe: 'd  d  |' , bonus:5, gives: {'leather':0.1, 'porkchop':0.1, 'egg':0.1, 'feather':0.1, 'wool':0.1}                          },
+      stone_sword :     {display:'Stone Sword',     profession: 'adventurer', recipe: 'C  C  |' , bonus:2, gives: {'porkchop':0.1, 'egg':0.1, 'string':0.05}                                                     },
+      iron_sword :      {display:'Iron Sword',      profession: 'adventurer', recipe: 'i  i  |' , bonus:3, gives: {'porkchop':0.1, 'egg':0.1, 'string':0.1, 'feather':0.1, 'leather':0.1}                        },
+      gold_sword :      {display:'Golden Sword',    profession: 'adventurer', recipe: 'g  g  |' , bonus:4, gives: {'porkchop':0.1, 'egg':0.1, 'string':0.2, 'feather':0.1, 'leather':0.1, 'wool':0.1}            },
+      diamond_sword :   {display:'Diamond Sword',   profession: 'adventurer', recipe: 'd  d  |' , bonus:5, gives: {'porkchop':0.1, 'egg':0.1, 'string':0.2, 'feather':0.1, 'leather':0.1, 'wool':0.1, 'milk':0.1}},
       wooden_shovel :   {display:'Wooden Shovel',   profession: 'labourer',   recipe: 'P  |  |' , bonus:1, gives: {'dirt':0.5}                                                                                   },
       stone_shovel :    {display:'Stone Shovel',    profession: 'labourer',   recipe: 'C  |  |' , bonus:2, gives: {'dirt':0.8,'flint':0.01,'gravel':0.1, 'sand':0.2}                                             },
       iron_shovel :     {display:'Iron Shovel',     profession: 'labourer',   recipe: 'i  |  |' , bonus:3, gives: {'dirt':0.8,'flint':0.01,'gravel':0.1, 'sand':0.2}                                             },
