a4b51b98447eff4098d5c65135ba2fc127b77881
fredley <fredley@gmail.com>
2014-08-05 20:48:01 +0000

    Sort out intersection of buildings

--- js/settlements.js
+++ js/settlements.js
@@ -2,7 +2,27 @@ var settlements = {
 
   occupied : [],
 
-  generate(x,y){
-
+  generate : function(height,width){
+    return {
+      height: height,
+      width: width,
+      buildings: [] // e.g. { type: 'house', x: 0, y: 0 }
+    };
   },
+
+  building_intersects : function(x,y,a,settlement){
+    a.right  = x + a.width;
+    a.bottom = y + a.height;
+    for(var b in settlement.buildings){
+      var building = buildings.getBuilding(slug);
+      b.right  = b.x + building.width;
+      b.bottom = b.y + building.height;
+        if (a.x < b.right &&
+            b.x < a.right &&
+            a.y < b.bottom &&
+            b.y < a.bottom) return true;
+    }
+    return false;
+  }
+
 };
