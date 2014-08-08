ece1dcd0dce7c20d6088c54822c6212746906244
fredley <fredley@gmail.com>
2014-08-08 19:07:28 +0000

    Remove dead code

--- js/settlements.js
+++ js/settlements.js
@@ -36,21 +36,6 @@ var settlements = {
     buildings.build(building);
   },
 
-  building_intersects : function(x,y,a,settlement){
-    a.right  = x + a.width;
-    a.bottom = y + a.height;
-    for(var b in settlement.buildings){
-      var building = buildings.getBuilding(slug);
-      b.right  = b.x + building.width;
-      b.bottom = b.y + building.height;
-        if (a.x < b.right &&
-            b.x < a.right &&
-            a.y < b.bottom &&
-            b.y < a.bottom) return true;
-    }
-    return false;
-  },
-
   draw_buildings : function(settlement){
     for(var b in settlement.buildings){
       var obj = buildings.getBuilding(settlement.buildings[b].building);
