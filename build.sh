d179011d77bb2e36b057ab503c4d116a07ee338d
fredley <fredley@gmail.com>
2014-08-25 17:38:01 +0100

    Add CSS processing to build script

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ cd js
 rm incraftmental.js
 cat *.js >> incraftmental.js
 uglifyjs incraftmental.js -e -m -c -o incraftmental.js
+cd ../css
+sass --style compressed style.scss:style.css
+sed -i '/sourceMappingURL/d' style.css
