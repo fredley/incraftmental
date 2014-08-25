01c7c957803182c0ec8bb9d781704223ed7a194a
fredley <fredley@gmail.com>
2014-08-25 18:57:28 +0100

    Add new buildings

--- js/buildings.js
+++ js/buildings.js
@@ -4,13 +4,9 @@ var buildings = {
 
   objects : {
     house        : { width: 2, height: 2,   symbol: 'H', houses: 0,  cost: {stone: 100,   cobble: 200,   plank: 400,   glass: 50 },
-                    tick : function(){ //nop
-                    }},
+                    tick : function(){}},
     blacksmith   : {width: 4, height: 4,   symbol: '*', houses: 2,  cost: {stone: 500,   cobble: 10000,   plank: 1000,  wood:  1000},
-                    tick : function(){
-                      // increases smelting rate
-                      //nop
-                    }},
+                    tick : function(){}},
     mill         : { width: 4, height: 8,   symbol: 'B', houses: 1,  cost: {stone: 100,   cobble: 800,   plank: 1000,  wood:  500},
                     tick : function(){
                       if(inventory.objects.items.seeds.quantity > 100){
@@ -26,10 +22,7 @@ var buildings = {
                       }
                     }},
     church       : { width: 8, height: 4,   symbol: '^', houses: 2,  cost: {stone: 5000,  cobble: 1000,  plank:1000,   wood: 1000,  glass: 5000, gold_ingot: 100},
-                    tick : function(){
-                      // saves half of village in case of zombie attack
-                      //nop
-                    }},
+                    tick : function(){}},
     school       : { width: 6, height: 4,   symbol: 'S', houses: 2,  cost: {stone: 8000,  cobble: 1000,  plank:3000,   wood: 5000,  glass: 3000},
                     tick : function(){
                       // upgrade some villagers to max. level 3
@@ -46,15 +39,12 @@ var buildings = {
                         }
                       }
                     }},
-    farm         : { width: 8, height: 8,   symbol: 'F', houses: 4,  cost: {stone: 1000,  cobble: 900,   plank:10000,  wood: 5000,  dirt: 50000, sand: 50000},
+    farm         : { width: 8, height: 8,   symbol: 'F', houses: 4,  cost: {stone: 1000,  cobble: 900,   plank:10000,  wood: 5000,  dirt: 50000, sand: 5000, bonemeal: 500},
                     tick : function(){
                       inventory.objects.items.wheat.quantity += 100;
                     }},
     barracks     : { width: 8, height: 8,   symbol: 'X', houses: 4,  cost: {stone: 10000, cobble: 900,   plank:1000,   wood: 5000,  iron_ingot: 1000, gold_ingot: 1000},
-                    tick : function(){
-                      // barracks protects village from zombie attack...
-                      //nop
-                    }},
+                    tick : function(){}},
     mine         : { width: 8, height: 8,   symbol: 'O', houses: 8,  cost: {stone: 10000, cobble: 10000, plank:10000,  wood: 10000, iron_ingot: 5000},
                     tick : function(){
                       for(var slug in this.objects.tools['diamond_pick'].gives){
@@ -64,13 +54,9 @@ var buildings = {
                       }
                     }},
     factory      : { width: 10, height: 6,  symbol: 'F', houses: 8,  cost: {stone: 20000, cobble: 10000, plank:10000,  wood: 10000, iron_ingot: 10000, gold_ingot: 5000, redstone: 1000},
-                    tick : function(){
-                      //bead machine?
-                    }},
+                    tick : function(){ /*TODO*/ }},
     market       : { width: 10, height: 10, symbol: '$', houses: 12, cost: {stone: 80000, cobble: 20000, plank:100000, wood: 10000, glass: 50000, iron_ingot: 20000, gold_ingot: 30000, redstone: 5000},
-                    tick : function(){
-                      //enables trading
-                    }},
+                    tick : function(){ /*TODO*/ }},
     university   : { width: 12, height: 10, symbol: 'U', houses: 16, cost: {stone: 100000,cobble: 80000, plank:120000, wood: 40000, glass: 80000, iron_ingot: 40000, gold_ingot: 80000, redstone: 10000},
                     tick : function(){
                      // upgrade some villagers to max. level 5
@@ -85,6 +71,32 @@ var buildings = {
                       villagers.population[randomChoice(elligible)].level++;
                       main.addAlert('One of your villagers was upgraded at university!');
                     }},
+    canteen      : { width: 4, height: 4, symbol: 'C', houses: 1, cost: {stone: 1000, cobble: 20000, plank:10000, wood: 10000, glass: 500},
+                    tick : function(){
+                      for(var group in inventory.objects){
+                        for(var object in inventory.objects[group]){
+                          var o = inventory.objects[group][object];
+                          if(o.cooks_to && o.quantity >= 10){
+                            inventory.addObject(object,-10);
+                            inventory.addObject(o.cooks_to,10);
+                          }
+                        }
+                      }
+                    }},
+    orchard      : { width: 8, height: 8, symbol: 'T', houses: 4, cost: {stone: 4000, cobble: 20000, plank:1000, wood: 100000, glass: 500, dirt: 10000, bonemeal: 500},
+                    tick : function(){
+                      for(var slug in this.objects.tools['diamond_axe'].gives){
+                        if(Math.random() < this.objects.tools['diamond_axe'].gives[slug]){
+                          inventory.addObject(slug,this.objects.tools[tool].bonus * this.objects.tools[tool].quantity * 100);
+                        }
+                      }
+                    }},
+    pig_sty      : { width: 8, height: 4, symbol: 'P', houses: 2, cost: {stone: 4000, cobble: 20000, plank:10000, wood: 10000, glass: 500, dirt: 50000, apple: 1000},
+                    tick : function(){
+                      inventory.addObject('porkchop',10);
+                    }},
+    kennel       : { width: 2, height: 2, symbol: 'K', houses: 1, cost: {cobble: 2000, plank:1000, wood: 1000, glass: 500},
+                    tick : function(){}},
   },
 
   canBuild : function(slug, settlement){
