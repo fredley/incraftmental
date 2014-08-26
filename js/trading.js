2e1557557868f0c98d6d3ce6bea0b2337e751131
fredley <fredley@gmail.com>
2014-08-26 21:48:07 +0100

    Add trading basics

---
+++ js/trading.js
@@ -0,0 +1,33 @@
+var trading = {
+  rates: {},
+  ticker: 0,
+  updateEvery: 30,
+
+  getRate: function(a,b){
+    if(a+'_'+b in this.rates){
+      return this.rates[a+'_'+b];
+    }else{
+      this.rates[a+'_'+b] = 50;
+      this.rates[b+'_'+a] = 50;
+    }
+  },
+
+  trade: function(a,b,n){
+    n = (n) ? n : 1;
+    var rate = this.getRate(a,b);
+    inventory.addObject(a,-100 * n);
+    inventory.addObject(b,rate * n);
+    this.rates[a+'_'+b] -= n;
+    this.rates[b+'_'+a] += n;
+  },
+
+  tick: function(){
+    if(ticker > updateEvery){
+      for(rate in this.rates){
+        if(this.rates[rate] < 50) this.rates++;
+        if(this.rates[rate] > 50) this.rates--;
+      }
+      ticker = 0;
+    }
+  }
+};
